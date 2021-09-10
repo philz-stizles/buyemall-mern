4d1ef57d1ccf7a95a34b56a8a762d0fd7517bf36
philz-stizles <theophilusighalo@gmail.com>
2021-09-10 02:43:42 +0100

    modify folder structyr

---
+++ buyemall-server/src/db/index.ts
@@ -0,0 +1,26 @@
+import mongoose, { Connection } from 'mongoose';
+
+const mongooseConnect = async (dbUri: string): Promise<void> => {
+  await mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection as Connection;
+
+  db.once('open', async () => {
+    console.log('Connected to database');
+  });
+
+  db.on('error', () => {
+    console.log('Error connecting to database');
+  });
+
+  db.on('error', () => {
+    console.log('Disconnected from database');
+  });
+};
+
+export default mongooseConnect;
